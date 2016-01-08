2c44ced26fb93f686f650c9bf8ca932d1e7b6d17
Imam Mohammad Bokhary <imam.bokhary@samsung.com>
2016-01-08 23:12:14 +0600

    1124: Celebrity Jeopardy

---
+++ 1124-Celebrity_jeopardy.cpp
@@ -0,0 +1,19 @@
+/*	
+ *	UVA 272
+ *	TEX Quotes
+ *	Author: Imam Mohammad Bokhary
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[10000];
+	while(gets(str))
+	{
+		cout<<str<<endl;
+	}
+ return 0;
+}
